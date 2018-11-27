05eed007b7863fe55cc2230bca43b673f8e74222
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2018-11-27 12:03:35 -0800

    create put method for user info

--- start/routes.js
+++ start/routes.js
@@ -23,3 +23,10 @@ Route.get('/', () => {
 Route.post('/signup', 'UserController.signup')
 
 Route.post('/login', 'UserController.login')
+
+Route.group(() => {
+  Route.get('/me', 'UserController.me')
+  Route.put('/update_profile', 'UserController.updateProfile')
+})
+  .prefix('account')
+  .middleware(['auth:jwt'])
