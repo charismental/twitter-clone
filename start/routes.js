e533aba95baacb94db16e6bf36c470201fbe049a
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2018-11-28 11:42:56 -0800

    favoriting

--- start/routes.js
+++ start/routes.js
@@ -44,6 +44,11 @@ Route.group(() => {
 
   Route.post('/tweet', 'TweetController.tweet').middleware(['auth:jwt'])
   Route.post('/tweets/reply/:id', 'TweetController.reply').middleware(['auth:jwt'])
+  Route.group(() => {
+    Route.post('/create', 'FavoriteController.favorite')
+  })
+    .prefix('favorites')
+    .middleware(['auth:jwt'])
 
   Route.get('/tweets/:id', 'TweetController.show')
 
